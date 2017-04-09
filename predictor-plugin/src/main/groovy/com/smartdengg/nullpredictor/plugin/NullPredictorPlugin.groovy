3c06141344f13b96168b829c4fac787c84f29846
SmartDengg <Hi4Joker@gmail.com>
2017-04-09 20:29:33 +0800

    New:  publish gradle plugin 1.0.1

--- predictor-plugin/src/main/groovy/com/smartdengg/nullpredictor/plugin/NullPredictorPlugin.groovy
+++ predictor-plugin/src/main/groovy/com/smartdengg/nullpredictor/plugin/NullPredictorPlugin.groovy
@@ -27,7 +27,7 @@ class NullPredictorPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-//      compile 'com.github.SmartDengg.nullpredictor:predictor-runtime:0.3.0'
+      compile project(':predictor-runtime')
       compile 'org.aspectj:aspectjrt:1.8.8'
     }
 
