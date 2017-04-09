4a0aa233cfe87e047ab9d875a96cbee7f8f8f7c0
SmartDengg <Hi4Joker@gmail.com>
2017-04-09 20:57:11 +0800

    New:  update inner gradle version

--- predictor-plugin/src/main/groovy/com/smartdengg/nullpredictor/plugin/NullPredictorPlugin.groovy
+++ predictor-plugin/src/main/groovy/com/smartdengg/nullpredictor/plugin/NullPredictorPlugin.groovy
@@ -27,7 +27,8 @@ class NullPredictorPlugin implements Plugin<Project> {
     }
 
     project.dependencies {
-      compile project(':predictor-runtime')
+      //      compile project(':predictor-runtime')
+      compile 'com.github.SmartDengg.nullpredictor:predictor-runtime:1.0.2'
       compile 'org.aspectj:aspectjrt:1.8.8'
     }
 
